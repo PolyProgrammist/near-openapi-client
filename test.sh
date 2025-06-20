78b05339756ed534b696330184549f5471291f23
Vadim Volodin <vad.e.volodin@gmail.com>
2025-06-20 06:41:06 +0000

    verbose test output

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ python3 tx.py --spec-fix
 cargo progenitor -i openapi.json -o near-openapi -n near-openapi -v 0.1.0
 cd near-openapi && cargo fmt && cd ..
 python3 tx.py --lib-fix
-cd example && mkdir target && cargo test > target/output.txt
+cd example && mkdir target && cargo -vv test
