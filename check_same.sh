fa6e679d1cf31d835da479253bf4046357630ad1
Vadim Volodin <vad.e.volodin@gmail.com>
2025-06-23 12:11:51 +0000

    fix check same

--- check_same.sh
+++ check_same.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-repo_client_checksum=$(tar c near-openapi-client | md5sum | awk '{print $1}')
-repo_types_checksum=$(tar c near-openapi-types | md5sum | awk '{print $1}')
+repo_client_checksum=$(md5sum near-openapi-client/Cargo.toml near-openapi-client/src/lib.rs | md5sum | awk '{print $1}')
+repo_types_checksum=$(md5sum near-openapi-types/Cargo.toml near-openapi-types/src/lib.rs | md5sum | awk '{print $1}')
 
 rm -rf near-openapi-client near-openapi-types
 ./gen.sh
 
-generated_client_checksum=$(tar c near-openapi-client | md5sum | awk '{print $1}')
-generated_types_checksum=$(tar c near-openapi-types | md5sum | awk '{print $1}')
+generated_client_checksum=$(md5sum near-openapi-client/Cargo.toml near-openapi-client/src/lib.rs | md5sum | awk '{print $1}')
+generated_types_checksum=$(md5sum near-openapi-types/Cargo.toml near-openapi-types/src/lib.rs | md5sum | awk '{print $1}')
 
 if [ "$repo_client_checksum" != "$generated_client_checksum" ] || [ "$repo_types_checksum" != "$generated_types_checksum" ]; then
     echo "The crates are not up to date. Please run 'cargo progenitor' to update it."
