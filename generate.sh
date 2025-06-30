473f6473197bbdd3152d9840059ecbbf18a8a43f
Vadim Volodin <vad.e.volodin@gmail.com>
2025-06-30 07:31:16 +0000

    tmp

--- generate.sh
+++ generate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-repo_client_checksum=$(md5sum near-openapi-client/Cargo.toml near-openapi-client/src/lib.rs | md5sum | awk '{print $1}')
-repo_types_checksum=$(md5sum near-openapi-types/Cargo.toml near-openapi-types/src/lib.rs | md5sum | awk '{print $1}')
-
 curl https://raw.githubusercontent.com/near/nearcore/refs/heads/master/chain/jsonrpc/openapi/progenitor.json > openapi.json
 python3 progenitor_fixes.py --spec-fix
 cargo progenitor -i openapi.json -o near-openapi -n near-openapi -v 0.0.0
@@ -12,13 +9,3 @@ cd near-openapi && cargo fmt && cd ..
 python3 progenitor_fixes.py --lib-fix
 cd near-openapi-client && cargo fmt && cd ..
 cd near-openapi-types && cargo fmt && cd ..
-
-generated_client_checksum=$(md5sum near-openapi-client/Cargo.toml near-openapi-client/src/lib.rs | md5sum | awk '{print $1}')
-generated_types_checksum=$(md5sum near-openapi-types/Cargo.toml near-openapi-types/src/lib.rs | md5sum | awk '{print $1}')
-
-if [ "$repo_client_checksum" != "$generated_client_checksum" ] || [ "$repo_types_checksum" != "$generated_types_checksum" ]; then
-    echo "The crates are not up to date. Please run './generate.sh' to update it."
-    exit 1
-else
-    echo "The crates are up to date."
-fi
